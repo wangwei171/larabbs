bf485e9f48a3e7b4c313aaca1fbeddb5250abbbe
wangwei171 <wangwei171@126.com>
2021-02-09 22:13:16 +0800

    配置缓存处理

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
 			<div class="card">
-				<img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" class="card-img-top" alt="{{$user->name}}">
+				<img src="{{$user->avatar}}" class="card-img-top" alt="{{$user->name}}">
 				<div class="card-body">
 					<h5><strong>个人简介</strong></h5>
 					<p>{{$user->introduction}} </p>
