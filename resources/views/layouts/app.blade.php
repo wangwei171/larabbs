dcb6c481afd5207ceaa8e5dda3e248766667e6e4
wangwei171 <wangwei171@126.com>
2021-02-16 10:51:26 +0800

    显示帖子

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<meta name="csrf-token" content="{{  csrf_token() }}">
 	<title>@yield('title','laraBBS')-进阶</title>
+	<meta name="description" content="@yield('description','laraBBS进阶')">
 	<link rel="stylesheet" type="text/css" href="{{  mix('css/app.css') }}">
 
 	@yield('styles')
